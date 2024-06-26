f4b18f40f580c6a4381ef10a82e549fd024fa59c
Prajapati Arjun <127772661+ArjunPraja@users.noreply.github.com>
2024-06-26 15:11:14 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const registerUser = require('../controller/registerUser');
-
-const router = express.Router();
-
-// Create User API
-router.post('/register', registerUser);
-
-module.exports = router;
